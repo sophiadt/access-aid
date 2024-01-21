15c70e624c26e70c420c212adadcf08b7772a4c4
syd3 <sophiadnth@gmail.com>
2024-01-20 23:11:40 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import {
-  getVenue,
+  getVenueMaker,
   showVenue,
   TGetVenueMakerOptions,
   MapView,
   MappedinMap,
   Mappedin,
-  E_SDK_EVENT
+  E_SDK
 } from "@mappedin/mappedin-js";
 import "@mappedin/mappedin-js/lib/mappedin.css";
+import "./style.css";
 
 // See Trial API key Terms and Conditions
 // https://developer.mappedin.com/guides/api-keys
@@ -27,6 +28,26 @@ selectorDiv.appendChild(mapGroupSelectElement);
 selectorDiv.appendChild(mapLevelSelectElement);
 selectorDiv.style.cssText = "position: fixed; top: 1rem; left: 1rem;";
 
+//Switch to the map with the selected id.
+function onLevelChange(event: Event) {
+  const id = (event.target as HTMLSelectElement).value;
+  mapView.setMap(id);
+}
+
+function populateMaps(maps: MappedinMap[]) {
+  mapLevelSelectElement.innerHTML = "";
+  mapLevelSelectElement.onchange = onLevelChange;
+
+  // Add each map as an option to the level select.
+  maps.forEach((map) => {
+    const option = document.createElement("option");
+    option.text = map.name;
+    option.value = map.id;
+    mapLevelSelectElement.add(option);
+  });
+  // Set the initial value of the level selector to the current map.
+  mapLevelSelectElement.value = mapView.currentMap.id;
+}
 
 function populateMapGroups() {
   mapGroupSelectElement.innerHTML = "";
@@ -56,49 +77,18 @@ mapGroupSelectElement.onchange = async (ev: Event) => {
   populateMaps(maps);
 };
 
-function onLevelChange(event: Event) {
-  const id = (event.target as HTMLSelectElement).value;
-  mapView.setMap(id);
-}
-
-function populateMaps(maps: MappedinMap[]) {
-  mapLevelSelectElement.innerHTML = "";
-  mapLevelSelectElement.onchange = onLevelChange;
-
-  // Add each map as an option to the level select.
-  maps.forEach((map) => {
-    const option = document.createElement("option");
-    option.text = map.name;
-    option.value = map.id;
-    mapLevelSelectElement.add(option);
-  });
-  // Set the initial value of the level selector to the current map.
-  mapLevelSelectElement.value = mapView.currentMap.id;
-}
-
 async function init() {
-  const options: TGetVenueOptions = {
-    mapId: "659efcf1040fcba69696e7b6",
-    key: "65a0422df128bbf7c7072349",
-    secret: "5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4",
-    venue: "your-venue-id", // Replace "your-venue-id" with the actual venue ID
-  };
-
-  venue = await getVenue(options);
-  mapView = await showVenue(document.getElementById("app")!, venue);
+  //Wait while the venue is downloaded.
+  venue = await getVenueMaker(options);
+  //Display the default map in the app div.
+  mapView = await showVenue(document.getElementById("app")!, venue, {
+    multiBufferRendering: true,
+    outdoorView: {
+      enabled: true
+    }
+  });
 
   populateMapGroups();
-
-  //Watch for map changes
-  mapView.on(E_SDK_EVENT.MAP_CHANGED, (map) => {
-    const mapGroup = map.mapGroup;
-    // Update select elements
-    if (mapGroup) {
-      mapGroupSelectElement.value = mapGroup.id;
-      populateMaps(mapGroup.maps);
-    }
-    mapLevelSelectElement.value = map.id;
-  });
 }
 
 init();
